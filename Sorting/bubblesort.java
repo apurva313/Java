2a7e746d91183794973171f6eaa1b793590ac9e8
Apurva Kumar <apurvakumar313@gmail.com>
2023-08-17 20:52:33 +0530

    Added Bubble_Sort

---
+++ Sorting/bubblesort.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class bubblesort {
+
+    public static void bubble(int arr[], int n) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+
+                if (arr[j] < arr[j + 1]) {
+                    int temp;
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+            }
+        }
+
+    }
+
+    public static void print(int arr[], int n) {
+        for (int i = 0; i < n - 1; i++) {
+            System.out.print(arr[i] + " ");
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 0, 2, 1, 2, 0 };
+        int n = arr.length;
+
+        System.out.print("Before Sorting: ");
+        print(arr, n);
+
+        bubble(arr, n);
+
+        System.out.print("\nAfter Sorting: ");
+        print(arr, n);
+    }
+}
