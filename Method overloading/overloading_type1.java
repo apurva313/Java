2236cffed4951ceee70a2c63889836cc2256eb63
Apurva Kumar <apurvakumar313@gmail.com>
2023-08-23 01:03:17 +0530

    update

--- Method overloading/overloading_type1.java
+++ Method overloading/overloading_type1.java
@@ -1,19 +1,19 @@
 // Achieving method overloading using same method name but differnt parameters
 
-
 class overloading_type1 {
-void method(int x,int y){
-int sum =x+y;
-System.out.println("The sum of " +x+ "+"+y+ "is " +sum);
-}
+    void method(int x, int y) {
+        int sum = x + y;
+        System.out.println("The sum of " + x + "+" + y + "is " + sum);
+    }
 
-void method(int x,int y,int z){
-int add=x+y+x;
-System.out.println("The sum of " +x+ "+"+y+"+"+z+ "is " +add);
-}
-public static void main(String[] args){
-overloading_type1 ob =new overloading_type1();
-ob.method(4,6);
-ob.method(8,2,5);
-}
+    void method(int x, int y, int z) {
+        int add = x + y + x;
+        System.out.println("The sum of " + x + "+" + y + "+" + z + "is " + add);
+    }
+
+    public static void main(String[] args) {
+        overloading_type1 ob = new overloading_type1();
+        ob.method(4, 6);
+        ob.method(8, 2, 5);
+    }
 }
