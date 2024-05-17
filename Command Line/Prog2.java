419791af6b34c152f6593efe3430c8346cebbbd4
apurva313 <apurvakumar313@gmail.com>
2024-05-18 02:00:44 +0530

    Added Command Line Program

---
+++ Command Line/Prog2.java
@@ -0,0 +1,8 @@
+class Prog2{
+public static void main (String[] args) {
+System.out. println(args[0]);
+System.out. println(args[1]);
+System.out. println(args[2]);
+System.out. println(args[3]);
+}
+}
