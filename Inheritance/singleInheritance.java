46746219ebd5c8ec6307f91e880f985eeb543a8a
apurva313 <apurvakumar313@gmail.com>
2024-12-03 11:36:11 +0530

    Added some method child and parent class

--- Inheritance/singleInheritance.java
+++ Inheritance/singleInheritance.java
@@ -1,14 +1,16 @@
 class A {
+    String name="V8";
     void meth1() {
         System.out.println("I'm Parent.");
     }
 }
 
 class B extends A {
+    String name="V12";
     void meth2() {
         System.out.println("I'm Child.");
     }
-    void meth1() {
+    void meth1(int a) {
         System.out.println("I'm Overriden.");
     }
 }
@@ -16,11 +18,15 @@ class B extends A {
 public class singleInheritance {
     public static void main(String[] args) {
         B obj = new B();
-        obj.meth1(); 
+        obj.meth1();
+        obj.meth1(5);
         obj.meth2();
+        System.out.println(obj.name);
     }
 }
 
 //Output:
+//I'm Parent.
 //I'm Overriden.
 //I'm Child.
+//V12
